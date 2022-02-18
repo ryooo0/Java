1bced89aa0af33b5ce22c6641cae0e06e3b1f1d4
ryooookada <btbl70120@gmail.com>
2022-02-18 16:35:42 +0900

    practice

---
+++ Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+        String name = "りょう";
+        System.out.println(name);
+        
+        name = "りょううう";
+        System.out.println(name);
+        
+        final int NAME = 10;
+        System.out.println(NAME);
+    }
+}
---
+++ Chapter05.java
@@ -0,0 +1,22 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long lon = 1222222222L;
+		System.out.println(lon);
+		double dou = 12.2;
+		System.out.println(dou);
+		boolean boo = true;
+		System.out.println(boo);
+
+		Float flo = 1.1111F;
+		System.out.println(flo);
+		Integer inte = 100;
+		System.out.println(inte);
+		Character chara = 'あ';
+		System.out.println(chara);
+		
+		int[] array = { 0, 10, 20, 30, 40 };
+        System.out.println(array[3]);
+      
+    }
+}
