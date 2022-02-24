c03f4dc0334b13b326a20b61c22d1ff76dfaa4a3
ryooookada <btbl70120@gmail.com>
2022-02-24 15:49:24 +0900

    practice

--- progate/progate1.java
+++ progate/progate1.java
@@ -95,5 +95,16 @@ public class progate1 {
 	    boolean bool = 3 * 9 == 27;
 	    // 変数boolの値を出力してください
 	    System.out.println(bool);
+	    
+	    // trueと出力されるようにしてください
+	    System.out.println(true || false);
+	    // falseと出力されるようにしてください
+	    System.out.println(false && true);
+	    // 「8 < 5」かつ「3 >= 2」の結果を出力してください
+	    System.out.println(8 < 5 && 3 >= 2);
+	    // 「8 < 5」または「3 >= 2」の結果を出力してください
+	    System.out.println(8 < 5 || 3 >= 2);
+	    // 「8 < 5」でない、の結果を出力してください
+	    System.out.println(!(8 < 5));
 	}
 }
