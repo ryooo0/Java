d23fd43c629292d3c0ae89c136001f5d39b92e36
ryooookada <btbl70120@gmail.com>
2022-02-26 22:56:17 +0900

    practice

--- progate/progate3.java
+++ progate/progate3.java
@@ -7,6 +7,11 @@ public class progate3 {
 	    // person1に対して、helloメソッドを呼び出してください
 	    // インスタンスメソッドを使用
 	    person1.hello();
+	     
+	    // person1のインスタンスフィールドnameに「Kate Jones」をセットしてください
+	    person1.name = "Kate Jones";
+	    // person1のインスタンスフィールドnameの値を出力してください
+	    System.out.println(person1.name);
 	    
 	}
 }
@@ -17,4 +22,6 @@ class Person {
 	public void hello() {
 	  System.out.println("こんにちは");
 	}
+	// インスタンスフィールドnameを定義してください
+	public String name;
 }
