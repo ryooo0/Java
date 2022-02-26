0e5e0275cbd782d3a76ac8957ff5ff04f9b1946b
ryooookada <btbl70120@gmail.com>
2022-02-26 21:55:13 +0900

    practice

---
+++ progate/progate3.java
@@ -0,0 +1,10 @@
+package progate;
+
+public class progate3 {
+	public static void main(String[] args) {
+	    // Personクラスのインスタンスを生成し、変数person1に代入してください
+	    Person person1 = new Person();
+	    // Personクラスのインスタンスを生成し、変数person2に代入してください
+	    Person person2 = new Person();
+	}
+}
