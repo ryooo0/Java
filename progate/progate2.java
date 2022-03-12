d36d2aae8a568a427e924064409ef8f6cffa7d76
ryooookada <btbl70120@gmail.com>
2022-03-12 22:04:15 +0900

    practice

--- progate/progate2.java
+++ progate/progate2.java
@@ -69,9 +69,7 @@ class Main2 {
 		System.out.println("年齢は" + age + "歳です");
 		System.out.println("身長は" + height + "mです");
 		System.out.println("体重は" + weight + "kgです");
-		// 変数bmiに、bmiメソッドを用いてBMIを代入してください
 		double bmi = bmi(height, weight);
-		// 変数bmiを用いて、「BMIは◯◯です」と出力してください
 		System.out.println("BMIは" + bmi + "です");
 	}
 
@@ -83,7 +81,6 @@ class Main2 {
 		return firstName + " " + middleName + " " + lastName;
 	}
 
-	// bmiメソッドを定義してください
 	public static double bmi(double height, double weight) {
 		return weight / height / height;
 	}
@@ -101,7 +98,6 @@ class Main3 {
 	    System.out.println("体重は" + weight + "kgです");
 	    double bmi = bmi(height, weight);
 	    System.out.println("BMIは" + bmi + "です"); 
-	    // isHealthyメソッドの結果で条件分岐を行ってください
 	    if (isHealthy(bmi)) {
 	      System.out.println("健康です");
 	    } else {
@@ -117,7 +113,6 @@ class Main3 {
 	  public static double bmi(double height, double weight) {
 	    return weight / height / height;
 	  }
-	  // isHealthyメソッドを定義してください
 	  public static boolean isHealthy(double bmi) {
 	    if (bmi >= 18.5 && bmi < 25.0) {
 	      return true;
@@ -133,14 +128,13 @@ class Main3 {
 System.out.println(Math.round(bmi));
 
 //java.util.Scannerを読み込んでください
-//import java.util.Scanner;
+  //import java.util.Scanner;
 class Main4 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("名前： ");
 		// 変数nameを定義し、コンソールから文字列を受け取って代入してください
 		String name = scanner.next();
-		// 「こんにちは◯◯さん」と出力してください
 		System.out.print("こんにちは" + name + "さん");
 	}
 }
@@ -149,19 +143,14 @@ class Main5 {
 	  public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("名前：");
-		// 文字列の入力を受け取ってください
 		String firstName = scanner.next();
 		System.out.print("名字：");
-		// 文字列の入力を受け取ってください
 		String lastName = scanner.next();
 		System.out.print("年齢：");
-		// 整数の入力を受け取ってください
 		int age = scanner.nextInt();
 		System.out.print("身長(m)：");
-		// 小数の入力を受け取ってください
 		double height = scanner.nextDouble();
 		System.out.print("体重(kg)：");
-		// 小数の入力を受け取ってください
 		double weight = scanner.nextDouble();
 		Person.printData(Person.fullName(firstName, lastName), age, height, weight);
 	  }
