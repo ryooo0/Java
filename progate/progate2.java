fa592de201ef073f77c8d6829c22c356e6bfd455
ryooookada <btbl70120@gmail.com>
2022-03-12 21:45:56 +0900

    practice

--- progate/progate2.java
+++ progate/progate2.java
@@ -41,7 +41,6 @@ class Main {
 class Main1 {
 	public static void main(String[] args) {
 		printData(fullName("Kate", "Jones"), 27);
-		// fullNameメソッドを用いて、printDataの引数を書き換えてください
 		printData(fullName("John", "Christopher", "Smith"), 65);
 	}
 
@@ -54,7 +53,6 @@ class Main1 {
 		return firstName + " " + lastName;
 	}
 
-	// fullNameメソッドを定義してください
 	public static String fullName(String firstName, String middleName, String lastName) {
 		return firstName + " " + middleName + " " + lastName;
 	}
