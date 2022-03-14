4b20c8f326fcf81c8405813d6dd66164d5b7133f
ryooookada <btbl70120@gmail.com>
2022-03-14 14:09:06 +0900

    practice

--- dmm/practice4.java
+++ dmm/practice4.java
@@ -25,5 +25,49 @@ public class practice4 {
         default:  // 全てのcaseの値に一致しない場合に実行
             System.out.println("がんばろう！");
         }
+        
+        
+        int number2 = 1;
+        while (number2 < 50) {  // while文：ループ開始,numberが50未満の場合繰り返す
+            number2 *= 2;
+            System.out.println("While01 = " + number2);
+        }
+        
+        int number3 = 1;
+        do {  // do-while文：ループ開始
+            number3 *= 2;
+            System.out.println("DoWhile01 = " + number3);
+        } while (number3 < 50);  // numberが50未満の場合繰り返す
+        
+        
+        // for文：
+        // 初期化式：number4を1で初期化
+        // 条件式：number4が5未満の場合、ブロックの処理を行う
+        // 変化式：ブロックの処理後に、number4をインクリメント
+        for (int number4 = 1; number4 <= 5; number4++) {
+            System.out.println("For01 = " + number4);
+        }
+        
+        // 拡張for文：
+        int[] array = { 1, 2, 3, 4, 5 };
+        for (int number5 : array) {  // 配列の要素を順番にnumber5に代入し、要素分繰り返す
+            System.out.println("For02 = " + number5);
+        }
+        
+        // break文：
+        for (int count = 0; count < 5; count++) {
+            if (count == 2) {
+                break;  // countが2になったら、for文を終了
+            }
+            System.out.println("Break01 = " + count);
+        }
+        
+        // continue文
+        for (int count1 = 0; count1 < 5; count1++) {
+            if (count1 == 2) {
+                continue;  // countが2の場合、この後の処理をスキップ
+            }
+            System.out.println("Continue01 = " + count1);
+        }
 	}
 }
---
+++ dmm/practice6.java
@@ -0,0 +1,40 @@
+package dmm;
+
+public class practice6 {
+	public static void main(String[] args) {
+        Human03 yamada = new Human03();
+
+        System.out.println("私の名前は、" + yamada.name + "です。");
+        String profile = yamada.getProfile();
+        System.out.println(profile + "です。");
+
+        yamada.greet("田辺");
+        yamada.greet(null);
+    }
+}
+
+public class Human03 {
+    public String name;
+    public int age;
+    public String profession;
+
+    public Human03() {
+        this.name = "山田";
+        this.age = 20;
+        this.profession = "プログラマー";
+    }
+
+    //String型の戻り値を返す引数なしのメソッドを定義
+    public String getProfile() {
+        return "年齢は" + this.age + "、職業は" + this.profession;
+    }
+
+    //戻り値を返さない、String型の引数を1つとるメソッドを定義
+    public void greet(String friend) {
+        if (friend == null) { //String型の値はnullの可能性があるのでチェック
+            System.out.println("挨拶する友達がわかりません！");
+            return;
+        }
+        System.out.println(friend + "さん、こんにちは！");
+    }
+}
